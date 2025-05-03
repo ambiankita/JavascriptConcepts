b33ebb26cacd2a247850333e09c4b27061e7bba1
Ankita Ambi <ambiankita9@gmail.com>
2025-05-03 12:38:58 +0530

    map method added

---
+++ Array/mapMethod.js
@@ -0,0 +1,9 @@
+const arr=[2,3,4,5,5,6]
+
+const newArr=arr.map((item)=>{item*2});
+
+console.log("New array:",newArr);
+
+
+const squaredArr=arr.map((item)=>item*item);
+console.log("Squared Array:",squaredArr);
